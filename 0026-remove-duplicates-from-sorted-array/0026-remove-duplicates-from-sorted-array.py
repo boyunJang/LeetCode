e32f6629992aec11d155b782532a3064d852975e
boyunJang <gobook1234@naver.com>
2024-05-12 21:07:20 +0900

    Time: 157 ms (5.5%), Space: 18.1 MB (24.04%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -0,0 +1,11 @@
+from collections import Counter
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        num_dic = Counter(nums)
+        for key, val in num_dic.items():
+            while val > 1:
+                nums.remove(key)
+                val -= 1
+
+        return len(nums)
